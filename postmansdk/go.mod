81a0b7cb601fbc0740ab3b64858220d9a0735b8b
Piyush Daga <piyushdaga5@gmail.com>
2023-03-28 16:06:09 +0530

    Renamed src -> postmansdk, working gin instrumentation

---
+++ postmansdk/go.mod
@@ -0,0 +1,16 @@
+module github.com/postmanlabs/postmansdk
+
+go 1.20
+
+require (
+	go.opentelemetry.io/otel v1.14.0
+	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.14.0
+	go.opentelemetry.io/otel/sdk v1.14.0
+)
+
+require (
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	go.opentelemetry.io/otel/trace v1.14.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+)
---
+++ postmansdk/main.go
@@ -0,0 +1,40 @@
+package postmansdk
+
+import (
+	"context"
+	"log"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+)
+
+func Initialize(serviceName string) func(context.Context) error {
+
+	// Adding a stdout exporter
+	exporter, err := newExporter()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	resources, err := resource.New(
+		context.Background(),
+		resource.WithAttributes(
+			attribute.String("service.name", serviceName),
+			attribute.String("library.language", "go"),
+		),
+	)
+	if err != nil {
+		log.Println("Could not set resources: ", err)
+	}
+
+	otel.SetTracerProvider(
+		sdktrace.NewTracerProvider(
+			sdktrace.WithSampler(sdktrace.AlwaysSample()),
+			sdktrace.WithBatcher(exporter),
+			sdktrace.WithResource(resources),
+		),
+	)
+	return exporter.Shutdown
+}
