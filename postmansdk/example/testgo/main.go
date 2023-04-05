76f57a7bb36437e5b5409244ff1e9b07631edf04
Gaurav Mann <gaurav.mann@postman.com>
2023-04-05 11:41:26 +0530

    Fix example imports

--- postmansdk/example/testgo/main.go
+++ postmansdk/example/testgo/main.go
@@ -5,24 +5,21 @@ import (
 
 	"github.com/gin-gonic/gin"
 
-	pm "github.com/postmanlabs/postmansdk"
-	pminterfaces "github.com/postmanlabs/postmansdk/interfaces"
+	pm "github.com/postmanlabs/postman-go-sdk/postmansdk"
+	pminterfaces "github.com/postmanlabs/postman-go-sdk/postmansdk/interfaces"
 )
-
-
-
 
 func main() {
 
 	apiKey := "REPLACE-THIS"
 	collectionId := "REPLACE-THIS"
 
-	 cleanup := pm.Initialize(collectionId, apiKey, pminterfaces.WithReceiverBaseUrl("REPLACE THIS"))
-	 defer cleanup(context.Background())
+	cleanup := pm.Initialize(collectionId, apiKey, pminterfaces.WithReceiverBaseUrl("REPLACE THIS"))
+	defer cleanup(context.Background())
 
 	router := gin.Default()
-	
-        // Otel patch
+
+	// Otel patch
 	pm.InstrumentGin(router)
 
 	router.GET("/albums", getAlbums)
