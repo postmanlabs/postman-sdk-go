f0a85802ce755b4efb5f2dea8371ef8cdd71c9a0
Piyush Daga <piyushdaga5@gmail.com>
2023-04-13 22:39:06 +0530

    feat: Release process support in github workflows

--- postmansdk/main.go
+++ postmansdk/main.go
@@ -99,6 +99,7 @@ func (psdk *postmanSDK) installExportPipeline(
 			attribute.String(
 				pmutils.POSTMAN_COLLECTION_ID_ATTRIBUTE_NAME, psdk.Config.CollectionId,
 			),
+			attribute.String(pmutils.POSTMAN_SDK_VERSION_ATTRIBUTE_NAME, POSTMAN_SDK_VERSION),
 		),
 	)
 	if err != nil {
