13a4845475f4e35d080e9e9f45af4be48667e5c1
Piyush Daga <piyushdaga5@gmail.com>
2023-04-13 23:24:21 +0530

    fix: Removing trace_receiver_path from constants file, that was a relic of a merge conflcit

--- postmansdk/utils/constants.go
+++ postmansdk/utils/constants.go
@@ -3,6 +3,5 @@ package utils
 const (
 	POSTMAN_COLLECTION_ID_ATTRIBUTE_NAME = "postman.collection.id"
 	POSTMAN_SDK_ENABLE_ENV_VAR_NAME      = "POSTMAN_SDK_ENABLE"
-	TRACE_RECEIVER_PATH                  = "/traces"
 	POSTMAN_SDK_VERSION_ATTRIBUTE_NAME   = "postman.sdk.version"
 )
